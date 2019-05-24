5c14e85dfe79e32591ef7f0beb3fe00908be2a90
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-05-25 00:49:21 +0500

    init

---
+++ Benchmarker/Program.cs
@@ -0,0 +1,98 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Exporters;
+using BenchmarkDotNet.Running;
+using Methods;
+
+namespace Benchmarker
+{
+	[RPlotExporter, RankColumn]
+	public class ExceptionPerformanceTest
+	{
+		[Benchmark]
+		public void WithLoop_AllNotFound()
+		{
+			for (int i = 0; i < 10; i++)
+				MethodsUnderTest.ContainsValue_WithLoop("not_existing");
+		}
+
+		[Benchmark]
+		public void WithLoop_50PercentNotFound()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				if (i % 2 == 0)
+				{
+					MethodsUnderTest.ContainsValue_WithLoop("not_existing");
+				}
+				else
+				{
+					MethodsUnderTest.ContainsValue_WithLoop("name_100");
+				}
+			}
+		}
+
+		[Benchmark]
+		public void WithLoop_10PercentNotFound()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				if (i == 0)
+				{
+					MethodsUnderTest.ContainsValue_WithLoop("not_existing");
+				}
+				else
+				{
+					MethodsUnderTest.ContainsValue_WithLoop("name_100");
+				}
+			}
+		}
+
+		[Benchmark]
+		public void WithException_AllNotFound()
+		{
+			for (int i = 0; i < 10; i++)
+				MethodsUnderTest.ContainsField_WithException("not_existing");
+		}
+
+		[Benchmark]
+		public void WithException_50PercentNotFound()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				if (i % 2 == 0)
+				{
+					MethodsUnderTest.ContainsField_WithException("not_existing");
+				}
+				else
+				{
+					MethodsUnderTest.ContainsField_WithException("name_100");
+				}
+			}
+		}
+
+		[Benchmark]
+		public void WithException_10PercentNotFound()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				if (i == 0)
+				{
+					MethodsUnderTest.ContainsField_WithException("not_existing");
+				}
+				else
+				{
+					MethodsUnderTest.ContainsField_WithException("name_100");
+				}
+			}
+		}
+	}
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var summary = BenchmarkRunner.Run<ExceptionPerformanceTest>();
+		}
+	}
+}
---
+++ Methods/MethodsUnderTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Methods
+{
+	public static class MethodsUnderTest
+	{
+		private static readonly Dictionary<string, int> FieldsDictionary;
+		
+		private static readonly List<string> Keys;
+		
+		static MethodsUnderTest()
+		{
+			FieldsDictionary = Enumerable
+				.Range(0, 200)
+				.ToDictionary(k => $"name_{k}", v => v);
+
+			Keys = FieldsDictionary.Keys.ToList();
+		}
+		
+		public static bool ContainsValue_WithLoop(string fieldName)
+		{
+			for (int i = 0; i < FieldsDictionary.Count; i++)
+			{
+				if (Keys[i] == fieldName)
+					return true;
+			}
+
+			return false;
+		}
+		
+		public static bool ContainsField_WithException(string fieldName)
+		{
+			if (FieldsDictionary.TryGetValue(fieldName, out var val))
+			{
+				return true;
+			}
+			else
+			{
+				try
+				{
+					throw new IndexOutOfRangeException();
+				}
+				catch (Exception e)
+				{
+					return false;
+				}
+			}
+		}
+	}
+}
